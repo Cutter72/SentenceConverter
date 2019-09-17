64c6302ad492f3476473379c2e07cfe6d2e47f7f
Cutter72 <drelich_pawel@o2.pl>
2019-09-17 21:55:02 +0200

    merged classes XmlJaxbMarshal into XmlStreamWriter

--- src/main/java/pl/nordea/task/converter/xml/XmlStreamWriter.java
+++ src/main/java/pl/nordea/task/converter/xml/XmlStreamWriter.java
@@ -1,19 +1,42 @@
 package pl.nordea.task.converter.xml;
 
+import pl.nordea.task.model.Sentence;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class XmlStreamWriter {
     private FileOutputStream fileOutputStream;
+    private Marshaller marshaller;
 
     public XmlStreamWriter(FileOutputStream fileOutputStream) {
         this.fileOutputStream = fileOutputStream;
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(Sentence.class);
+            this.marshaller = jaxbContext.createMarshaller();
+            this.marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+            this.marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
     public void startDocument() {
         try {
             fileOutputStream.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<oryginalText>".getBytes());
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addSentence(Sentence sentence, FileOutputStream fileOutputStream) {
+        try {
+            this.marshaller.marshal(sentence, fileOutputStream);
+        } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
--- src/test/java/pl/nordea/task/converter/xml/XmlStreamWriterTest.java
+++ src/test/java/pl/nordea/task/converter/xml/XmlStreamWriterTest.java
@@ -1,13 +1,16 @@
 package pl.nordea.task.converter.xml;
 
 import org.junit.Test;
+import pl.nordea.task.model.Sentence;
 
+import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -33,6 +36,57 @@ public class XmlStreamWriterTest {
         }
         //when
         xmlStreamWriter.startDocument();
+        List<String> actual = null;
+        try {
+            actual = Files.readAllLines(outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addSentence() {
+        //given
+        Path outputFile = Paths.get("src/main/resources/test_data/sentenceXmlWriteTest").toAbsolutePath();
+        Path expectedFile = Paths.get("src/main/resources/test_data/expectedSentenceXmlWriteTest").toAbsolutePath();
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(outputFile.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        XmlStreamWriter xmlStreamWriter;
+        xmlStreamWriter = new XmlStreamWriter(outputStream);
+
+        Sentence sentenceA = new Sentence();
+        List<String> wordListA = new ArrayList<>();
+        wordListA.add("Love");
+        wordListA.add("is");
+        wordListA.add("everything");
+        wordListA.add("you");
+        wordListA.add("need");
+        sentenceA.setWordList(wordListA);
+
+        Sentence sentenceB = new Sentence();
+        List<String> wordListB = new ArrayList<>();
+        wordListB.add("Be");
+        wordListB.add("yourself");
+        sentenceB.setWordList(wordListB);
+
+
+        List<String> expected = null;
+        try {
+            expected = Files.readAllLines(expectedFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //when
+
+        xmlStreamWriter.addSentence(sentenceA, outputStream);
+        xmlStreamWriter.addSentence(sentenceB, outputStream);
         List<String> actual = null;
         try {
             actual = Files.readAllLines(outputFile);
