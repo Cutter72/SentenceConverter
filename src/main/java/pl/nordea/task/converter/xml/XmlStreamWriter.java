43931d6000641f4339242855b9899598d32f258b
Cutter72 <drelich_pawel@o2.pl>
2019-09-17 21:29:10 +0200

    startDocument implemented

--- src/main/java/pl/nordea/task/converter/xml/XmlStreamWriter.java
+++ src/main/java/pl/nordea/task/converter/xml/XmlStreamWriter.java
@@ -1,6 +1,7 @@
 package pl.nordea.task.converter.xml;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class XmlStreamWriter {
     private FileOutputStream fileOutputStream;
@@ -10,6 +11,11 @@ public class XmlStreamWriter {
     }
 
     public void startDocument() {
+        try {
+            fileOutputStream.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<oryginalText>".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void closeDocument() {
